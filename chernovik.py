a518a47d92da19cd535283872e89626bebb622dc
Evgeny <bashmack530053@yandex.ru>
2023-05-07 21:51:01 +0300

    del trash

--- chernovik.py
+++
@@ -1,8 +0,0 @@
-num = 379
-num = str(num)
-first_inx = 0
-for i in range(0, len(num)):
-    for j in range(9, int(num[0]), -1):
-        new_num = num[:i] + str(j) + num[i+1:]
-        print(new_num)
-    new_num = num
